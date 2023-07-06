349aa4da9a35a7bce3a48db2d72c7361f4092a10
aaditikurlekar <aaditikurlekar1112@gmail.com>
2023-07-06 13:35:21 +0530

    UI - Profile page
    - add MUI icon dependency
    - add SASS css dependency
    - UI for profile page

--- src/meterial-ui-components/Button/ButtonComponent.jsx
+++ src/meterial-ui-components/Button/ButtonComponent.jsx
@@ -1,9 +1,9 @@
 import { Button } from "@mui/material";
 import React from "react";
 
-export default function ButtonComponent({className, buttonText, buttonImage, onClickHandler}){
+export default function ButtonComponent({className, buttonText, variant, buttonImage, onClickHandler}){
     return(
-        <Button className={className} variant="contained" onClick={(e) => onClickHandler(e)}>
+        <Button className={className} variant={variant} onClick={(e) => onClickHandler(e)}>
             {buttonText}
         </Button>
     )
